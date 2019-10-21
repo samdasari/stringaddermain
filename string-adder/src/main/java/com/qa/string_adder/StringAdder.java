659d4d06473902ad7a57138621a492d4ea0128b9
Admin <Admin@STUDENT03>
2019-10-21 13:19:08 +0100

    adder

---
+++ string-adder/src/main/java/com/qa/string_adder/StringAdder.java
@@ -0,0 +1,25 @@
+package com.qa.string_adder;
+
+public class StringAdder {
+	
+	public String add(String lh, String rh)
+	{
+		
+		
+	}
+
+}
+
+
+//phoneUtils("sam",9999999999);
+		//phoneUtils("dasari",8888888888);
+		
+		//public static void phoneUtils(String UserName , int number) {
+			
+			//String number;
+			
+			//if (number = )
+		//}
+
+//	System.out.println("Phone number 123-456-7890 x1234 validation result: "+validatePhoneNumber("123-456-7890 x1234"));
+//  System.out.println("Phone number 123-456-7890 ext1234 validation result: "+validatePhoneNumber("123-456-7890 ext1234"));
